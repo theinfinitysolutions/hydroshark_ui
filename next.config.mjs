10031f4186f60c3bbdad92bd453ce00217c19339
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-11 16:44:11 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["three"],
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.obj$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static/models/",
+            publicPath: "/_next/static/models/",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require("next/dist/build/swc");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,6 +13,46 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+        marqueebanner: "marquee 5s linear infinite",
+        slideUp: "slideUp 2s deplay-[1000ms] ease-in-out forwards",
+        rotate: "rotate 8s linear infinite",
+        bounce: "bounce1 2s infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translate3d(var(--move-initial), 0, 0)" },
+          "100%": { transform: "translate3d(var(--move-final), 0, 0)" },
+        },
+        slideUp: {
+          "0%": { opacity: 0.5, transform: "translateY(20vh) rotateX(0)" },
+
+          "100%": { opacity: 1, transform: "translateY(0) rotateX(-8deg)" },
+        },
+        rotate: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        bounce1: {
+          "0%": {
+            transform: "translateY(0)",
+            rotate: "rotate(8deg)",
+          },
+          "50%": {
+            transform: "translateY(-3vh)",
+            rotate: "rotate(8deg)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            rotate: "rotate(8deg)",
+          },
+        },
       },
     },
   },
