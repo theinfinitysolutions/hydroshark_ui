44db88fd5199f040576ef2e7f311e01e6b85f339
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-17 15:24:03 +0530

    added consts

---
+++ utils/consts.js
@@ -0,0 +1,20 @@
+export const flavorData = [
+  {
+    title: "LEMON",
+    description:
+      "The Lemon flavor is a classic, with a zesty and refreshing taste that is perfect for a hot summer day.",
+    id: 1,
+    quanity: "250ML",
+    ingredients:
+      " Carbonated water, Sugar, Stevia (INS 960), Acidity Regulator (INS 330), Class II Preservative (INS 211), Ascorbic Acid (Vit C), Niacin (Vit B3), Lemon Juice (1.8%), Mineral Salts, Vitamin premix (B2, B6, B12)",
+  },
+  {
+    title: "MANGO",
+    description:
+      "The Mango flavor is a tropical delight, with a sweet and tangy taste that is sure to please your taste buds.",
+    id: 2,
+    quanity: "250ML",
+    ingredients:
+      "Carbonated water, Sugar, Stevia (INS 960), Acidity Regulators (INS 330, INS 331), Class II Preservative (INS 211), Ascorbic Acid (Vit C), Niacin (Vit B3), LCLT (6 mg/ 100 ml), Inositol (2 mg / 100 ml), Mix Fruit Juice from Pulp (2%) (Mango, Guava, Apple, Passion Fruit, Apricot, Peach, Lemon), Mineral Salts, Stabiliser (INS415), Vitamin Premix (B2, B6, B12)",
+  },
+];
