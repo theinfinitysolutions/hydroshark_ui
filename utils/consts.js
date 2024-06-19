643c25a0ae68f1af4215a953c250441a7b568d56
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-19 22:48:33 +0530

    consts changes

--- utils/consts.js
+++ utils/consts.js
@@ -18,3 +18,42 @@ export const flavorData = [
       "Carbonated water, Sugar, Stevia (INS 960), Acidity Regulators (INS 330, INS 331), Class II Preservative (INS 211), Ascorbic Acid (Vit C), Niacin (Vit B3), LCLT (6 mg/ 100 ml), Inositol (2 mg / 100 ml), Mix Fruit Juice from Pulp (2%) (Mango, Guava, Apple, Passion Fruit, Apricot, Peach, Lemon), Mineral Salts, Stabiliser (INS415), Vitamin Premix (B2, B6, B12)",
   },
 ];
+
+export const whyUs = [
+  {
+    title: "Balanced Nutrition and Flavor",
+    description:
+      "Hydroshark is India’s first carbonated hydration drink that perfectly balances essential nutrients with delicious flavor. Our tasty formula makes healthy hydration enjoyable.",
+    image: "/icons1.png",
+  },
+  {
+    title: "Advanced Hydration Formula",
+    description:
+      "Our specially curated blend of vitamins, minerals, and electrolytes ensures ultimate and quicker hydration. This unique combination keeps you hydrated and energized all day.",
+    image: "/icons2.png",
+  },
+  {
+    title: "Comprehensive Well-being",
+    description:
+      "Hydroshark supports not just physical replenishment but also mental and emotional well-being. Our drink acts as a mood lifter, helping you feel better overall.",
+    image: "/icons3.png",
+  },
+  {
+    title: "Quality Ingredients",
+    description:
+      "We are committed to using only the highest quality raw materials. Our ingredients are sourced globally, including Indian Sugarcane, Stevia from Malaysia and world-famous Indian Mangoes. This ensures that only the best reaches our consumers.",
+    image: "/icons4.png",
+  },
+  {
+    title: "Healthier Choice",
+    description:
+      "By eliminating caffeine and reducing sugar content with stevia, Hydroshark offers a lower-calorie alternative suitable for everyone. We focus on providing a healthy and refreshing drink without compromising on taste.",
+    image: "/icons5.png",
+  },
+  {
+    title: "Top-Grade Production Standards",
+    description:
+      "Our production plant meets the standards of major beverage brands, ensuring no compromise on quality. For us, the health of our consumers is paramount.",
+    image: "/icons6.png",
+  },
+];
