0a03f3610e6cd7574aa77324e60bf5322163f057
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-19 23:25:06 +0530

    added config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2g3h1gpjmm5ra.cloudfront.net",
+      },
+    ],
+  },
   transpilePackages: ["three"],
   webpack: (config, { isServer }) => {
     config.module.rules.push({
