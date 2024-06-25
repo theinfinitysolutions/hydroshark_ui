6bb6c3d1e34e76701fc97a0141c65f6c7cc2c648
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-25 17:51:14 +0530

    home section ui changes

--- components/HomeSection3.jsx
+++ components/HomeSection3.jsx
@@ -15,13 +15,13 @@ const HomeSection3 = () => {
   return (
     <div
       ref={targetRef}
-      className=" flex flex-row justify-between  overflow-hidden w-full h-screen max-h-screen"
+      className=" flex flex-row justify-between  overflow-hidden w-full h-[60vh] lg:h-screen max-h-screen"
     >
-      <div className="flex w-[35%] h-full max-h-screen overflow-hidden">
+      <div className="hidden lg:flex w-[35%] h-full max-h-screen overflow-hidden">
         <ParallaxScroll targetRef={targetRef} />
       </div>
-      <div className=" relative flex flex-col items-start w-[65%] h-full px-8">
-        <div className="absolute -left-[75vw] -bottom-[0vh] overflow-hidden [--offset:40vw]  bg-black rotate-[90deg] z-40 [--move-initial:calc(-25%_+_var(--offset))] [--move-final:calc(-50%_+_var(--offset))] ">
+      <div className=" relative flex flex-col items-start w-screen lg:w-[65%] h-full px-4 lg:px-8">
+        <div className="hidden lg:block absolute -left-[75vw] -bottom-[0vh] overflow-hidden [--offset:40vw]  bg-black rotate-[90deg] z-40 [--move-initial:calc(-25%_+_var(--offset))] [--move-final:calc(-50%_+_var(--offset))] ">
           <div
             className="min-w-screen w-[150vw] flex flex-row py-2 border-y-[2px] border-white border-dotted items-center relative  transform-[translate3d(var(--move-initial),0,0)] animate-marqueebanner [animation-play-state:running]"
             aria-hidden="true"
@@ -61,86 +61,63 @@ const HomeSection3 = () => {
             <span className="text-sm p-[0_2vw] text-white">{"0g sugar"}</span>
           </div>
         </div>
-        <p className=" text-4xl text-[#3997a1] italic mt-[10vh]">
+        <p className=" text-xl lg:text-4xl text-[#3997a1] italic mt-[5vh] lg:mt-[10vh]">
           {"Experience the Hydroshark Difference "}
         </p>
-        <div className=" flex flex-row gap-x-2 justify-start items-center ">
+        <div className=" flex flex-row  gap-x-2 justify-start items-center ">
           <p
-            className={`${caveat.className} text-[3rem] mr-4 font-semibold col-span-2 text-black`}
+            className={`${caveat.className} text-[1.1rem] lg:text-[2.5rem] lg:mr-4 font-semibold text-black`}
           >
             Loved by{" "}
           </p>
-          <p className=" text-[4rem] font-semibold col-span-3 text-black">
-            ATHLETES
-          </p>
-          <div className="h-[8vh] w-[8vh] -mt-2 relative">
-            <Image
-              src={process.env.NEXT_PUBLIC_API_URL + "/asset2.svg"}
-              layout="fill"
-            />
+          <div className=" flex flex-row items-center gap-x-1 justify-start">
+            <p className=" text-[2rem] lg:text-[3rem] lg:text-[4rem] font-semibold  text-black">
+              ATHLETES
+            </p>
+            <div className=" h-[5vw] w-[5vw]  lg:h-[8vh] lg:w-[8vh] -mt-2 relative">
+              <Image
+                src={process.env.NEXT_PUBLIC_API_URL + "/asset2.svg"}
+                layout="fill"
+              />
+            </div>
           </div>
-          <p className=" text-[4rem] font-semibold col-span-3 text-black">
-            GAMERS
-          </p>
-          <div className="h-[6vh] w-[6vh] -mt-2 relative">
-            <Image
-              src={process.env.NEXT_PUBLIC_API_URL + "/asset3.png"}
-              layout="fill"
-            />
+          <div className=" flex flex-row items-center gap-x-1 justify-start">
+            <p className=" text-[2rem] lg:text-[3rem] lg:text-[4rem] font-semibold col-span-3 text-black">
+              GAMERS
+            </p>
+            <div className="h-[5vw] w-[5vw]  lg:h-[6vh] lg:w-[6vh] -mt-2 relative">
+              <Image
+                src={process.env.NEXT_PUBLIC_API_URL + "/asset3.png"}
+                layout="fill"
+              />
+            </div>
           </div>
         </div>
         <div className=" flex flex-row gap-x-2 justify-start items-center">
-          <div className="h-[8vh] w-[8vh]  -mt-2 relative">
+          <div className="h-[5vw] w-[5vw]  lg:h-[8vh] lg:w-[8vh]  -mt-2 relative">
             <Image
               src={process.env.NEXT_PUBLIC_API_URL + "/asset7.png"}
               layout="fill"
             />
           </div>
-          <p className=" text-[4rem] font-semibold col-span-3 text-black">
+          <p className=" text-[2rem] lg:text-[3rem] lg:text-[4rem] font-semibold col-span-3 text-black">
             DAREDEVILS
           </p>
-          <div className="h-[8vh] w-[8vh]   -mt-2 relative">
+          <div className="h-[5vw] w-[5vw]  lg:h-[8vh] lg:w-[8vh]   -mt-2 relative">
             <Image
               src={process.env.NEXT_PUBLIC_API_URL + "/asset5.png"}
               layout="fill"
             />
           </div>
         </div>
-        <div className="h-[15vh] w-[15vh] mt-4 animate-rotate absolute right-[10vh] bottom-[10vh]">
+        <div className=" h-[20vw] w-[20vw] lg:h-[15vh] lg:w-[15vh] mt-4 animate-rotate absolute right-8 bottom-8 lg:right-[10vh] lg:bottom-[10vh]">
           <Image
             src={process.env.NEXT_PUBLIC_API_URL + "/hydroshark.png"}
             layout="fill"
           />
         </div>
-        <div className=" h-[30vh]  w-[10vw] transform rotate-[-80deg] absolute right-[25vw] -bottom-[5vh] ">
-          <Image
-            src={process.env.NEXT_PUBLIC_API_URL + "/lemoncan.png"}
-            layout="fill"
-          />
-        </div>
-        <div className=" h-[30vh]  w-[10vw] transform rotate-[-60deg] absolute right-[22.5vw] bottom-[10vh] ">
-          <Image
-            src={process.env.NEXT_PUBLIC_API_URL + "/mangocan.png"}
-            layout="fill"
-          />
-        </div>
-        <div className=" h-[30vh]  w-[10vw] transform rotate-[-40deg] absolute right-[16vw] bottom-[25vh] ">
-          <Image
-            src={process.env.NEXT_PUBLIC_API_URL + "/lemoncan.png"}
-            layout="fill"
-          />
-        </div>
-        <div className=" h-[30vh]  w-[10vw] transform rotate-[-20deg] absolute right-[8vw] bottom-[35vh] ">
-          <Image
-            src={process.env.NEXT_PUBLIC_API_URL + "/mangocan.png"}
-            layout="fill"
-          />
-        </div>
-        <div className=" h-[30vh]  w-[10vw] transform rotate-[0deg] absolute -right-4 bottom-[37.5vh] ">
-          <Image
-            src={process.env.NEXT_PUBLIC_API_URL + "/lemoncan.png"}
-            layout="fill"
-          />
+        <div className=" absolute bottom-0 right-0 w-[80vw] h-[80vw] lg:h-[60vh] lg:w-[60vh]">
+          <Image src={"/curvedCans.png"} objectFit="cover" layout="fill" />
         </div>
       </div>
     </div>
