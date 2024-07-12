88d54dea508bc48b35939549e1332047309d5463
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-12 09:06:08 +0530

    pages layout changes

---
+++ app/athletes/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "Athletes | Hydroshark",
+  description: "India's First Carbonated Hydration Drink",
+};
+
+const Layout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
