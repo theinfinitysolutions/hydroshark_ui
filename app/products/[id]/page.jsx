8a5608d009228a0fa15d93ed39a881744cc0fda6
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-05 15:36:30 +0530

    products changes

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,21 +1,30 @@
 // import { getProducts } from "@/utils/helper";
+"use client";
 import { BASE_URL } from "@/utils/instance";
 import ViewProduct from "./_viewProduct";
 import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
-async function fetchProductIds() {
-  const response = await axios.get(`${BASE_URL}/drinks/product/`);
-  const products = response.data.results;
-  return products.map((product) => ({ id: product.id + "" }));
-}
+// async function fetchProductIds() {
+//   const response = await axios.get(`${BASE_URL}/drinks/product/`);
+//   const products = response.data.results;
+//   return products.map((product) => ({ id: product.id + "" }));
+// }
 
-// Static generation of paths
-export async function generateStaticParams() {
-  const ids = await fetchProductIds();
-  return ids;
-}
+// // Static generation of paths
+// export async function generateStaticParams() {
+//   const ids = await fetchProductIds();
+//   return ids;
+// }
 
-export default function ProductPage({ params }) {
+export default function ProductPage() {
+  const params = useParams();
+
+  useEffect(() => {
+    console.log("params", params);
+  }, [params]);
+
   return (
     <div>
       <ViewProduct id={params.id} />
