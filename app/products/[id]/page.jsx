f7e08a61fd479a9aaf5d039861de9ca348c869a0
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-06 18:38:15 +0530

    minor fixes

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -6,15 +6,18 @@ import axios from "axios";
 async function fetchProductIds() {
   const response = await axios.get(`${BASE_URL}/drinks/product/`);
   const products = response.data.results;
-  return products.map((product) => ({ id: product.id + "" })) || [];
+  return products.map((product) => ({ id: product.id + "" }));
 }
 
-// Static generation of paths
 export async function generateStaticParams() {
   const ids = await fetchProductIds();
   return ids;
 }
 
 export default function ProductPage({ params }) {
-  return <div>{params.id ? <ViewProduct id={params.id} /> : null}</div>;
+  return (
+    <div>
+      <ViewProduct id={params.id} />
+    </div>
+  );
 }
