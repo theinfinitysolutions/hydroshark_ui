686003701af602a41a91b0a96c1dada46638bc06
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-12 10:59:44 +0530

    minor fix

--- app/layout.js
+++ app/layout.js
@@ -110,7 +110,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+            fbq('init', '1066527391554625');
             fbq('track', 'PageView');
         `}
         </Script>
@@ -119,7 +119,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=1066527391554625&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
