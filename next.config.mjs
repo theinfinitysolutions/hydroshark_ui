2a47fbcabc3d41e68b8fed47fda71c5b335508ea
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-24 00:52:10 +0530

    nextconfig changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   reactStrictMode: false,
-  // trailingSlash: true,
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
