834387bc6a5b65c8b93dbcb3b33118df49ac91c1
infinitysol <theinfinitysolutions9@gmail.com>
2024-12-21 16:28:48 +0530

    drinks page changes

---
+++ app/products/drinks/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "Products | Hydroshark",
+  description: "India's First Caffeine Free Energy Drink",
+};
+
+const Layout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
